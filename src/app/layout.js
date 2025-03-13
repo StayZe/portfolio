b6d26d9811d814a73bf304d6e6f8a8b8c393a953
StayZe <mandra.antoine@gmail.com>
2025-03-13 10:11:54 +0100

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./fonts.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from "./components/header";
 import Footer from "./components/footer";
 
